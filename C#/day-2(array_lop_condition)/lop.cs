bcca9e1f33e96e730a12a7d08ac9c9b93433d837
mizan192 <miz1998an@gmail.com>
2023-03-14 03:39:47 +0600

    lop-aded

---
+++ C#/day-2(array_lop_condition)/lop.cs
@@ -0,0 +1,22 @@
+using System;
+namespace HelloWorldApplication{
+    class HellloWorld{
+        static void Main(){
+
+//using for lop
+            for (int i = 0; i <= 10; i = i + 2)
+      {
+        Console.WriteLine(i);
+      } 
+
+//using while lop
+	int i=0;
+	while(i<=12)
+      {
+        Console.WriteLine(i);
+	i++;	
+      }
+
+        }
+    }
+}
