d8a582ca6a93807f9fe8327bdc8ed78cffa69542
mizan192 <miz1998an@gmail.com>
2023-03-14 02:10:49 +0600

    basic-prob

---
+++ C#/day-2(array_lop_condition)/basic.cs
@@ -0,0 +1,13 @@
+using System;
+namespace HelloWorldApplication{
+    class HellloWorld{
+        static void Main(){
+
+            Console.WriteLine("Hello My World");  //output string
+           // Console.WriteLine("value is : ");
+
+            Console.ReadKey();    //read input
+
+        }
+    }
+}
