0c26424b2a7f48e9d43330c3e6a997af4fff77ae
mizan192 <miz1998an@gmail.com>
2023-04-08 18:49:39 +0600

    encapsulation

---
+++ C#/Encapsulation/Encapsulation.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Encapsulation
+{
+public class Person
+{
+    private string name;  //data members
+    private int age;//data members
+    private DateTime createdAt;
+    public Person()
+    {
+        this.createdAt = DateTime.Now;
+    }
+
+    public DateTime GetCreationDate()
+    {
+        return this.createdAt;
+    }
+    public string GetName()
+    {
+        return this.name;
+    }
+    public void SetName(string pName)
+    {
+        this.name = pName;
+    }
+    public int GetAge()
+    {
+        return this.age;
+    }
+
+    public void SetAge(int pAge)
+    {
+        if (pAge > 0 && pAge < 150)
+        {
+            this.age = pAge;
+        }
+        else
+        {
+            throw new Exception("Invalid age");
+        }
+    }
+
+
+    public void DisplayPersonDetails()  //members
+    {
+        Console.WriteLine($"name= {name}, age={age}");
+    }
+
+}
+
+public class Program
+{
+    static void Main()
+    {
+        Person p = new Person();
+        p.SetName("mizan");
+        p.SetAge(15);
+        Console.WriteLine($"Created at : {p.GetCreationDate()}");
+        p.DisplayPersonDetails();
+    }
+}
+}
